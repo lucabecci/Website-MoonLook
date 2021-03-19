9c279ae6da4e5c8d827eef4ed979b41a8cc53441
luca <beccibrian@gmail.com>
2021-03-19 16:38:01 -0300

    chore(Server): Docker-compose added for server and nginx

--- packages/server/src/config/config.ts
+++ packages/server/src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
-    PORT: process.env.PORT || 4000,
+    PORT: process.env.PORT || 5000,
     DB_URI: process.env.DB_URI || "mongodb://localhost/moonlook",
     STATUS: process.env.STATUS || "dev",
 };
