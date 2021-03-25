c4af5fd06d6f6f52b0737e69e5e760c3b4eebf5d
luca <beccibrian@gmail.com>
2021-03-25 15:45:08 -0300

    fix(Bin): spaces deleted in Docker.sh

--- bin/Docker.sh
+++ bin/Docker.sh
@@ -1,23 +1,13 @@
 echo "STARTED WITH DOCKER" 
 ##Check docker binaries
 echo "remove and check docker binaries" 
-sudo yum remove docker \
-                docker-client \
-                docker-client-latest \
-                docker-common \
-                docker-latest \
-                docker-latest-logrotate \
-                docker-logrotate \
-                docker-engine
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 ##install others for correct docker installation
 echo "install utils for docker" 
 sudo yum install -y yum-utils
 ##Config manager
 echo "config manager" 
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 ##install docker
 echo "install docker" 
 sudo yum install docker-ce docker-ce-cli containerd.io	
