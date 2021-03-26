d82a7548e79c24c97c6ff4f8097cb6229c7fd6a1
luca <beccibrian@gmail.com>
2021-03-26 12:12:40 -0300

    fix(Compose): config installation fixed for short file

--- bin/Compose.sh
+++ bin/Compose.sh
@@ -1,13 +1,9 @@
 echo "STARTED WITH COMPOSE BUILD" 
 
 #Get package
-echo "get binary of compose" 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$ uname -s -$ uname -m " -o /usr/local/bin/docker-compose
-
-#Exect
-echo "exec compose bin" 
-sudo chmod +x /usr/local/bin/docker-compose
+echo "Get binary of compose" 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 #Permissions
-echo "set permissions" 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "Permissions compose bin" 
+sudo chmod +x /usr/local/bin/docker-compose
